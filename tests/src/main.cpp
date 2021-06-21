4611bf24ce0ecf222bb29889b84ffbe52cf6d703
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-06-21 21:56:40 +0100

    Updates

---
+++ tests/src/main.cpp
@@ -0,0 +1,16 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/configuration/blob/main/LICENSE.txt
+*/
+
+#include <Ishiko/TestFramework/TestFrameworkCore.h>
+
+using namespace Ishiko::Tests;
+
+int main(int argc, char* argv[])
+{
+    TestHarness theTestHarness("IshikoConfiguration");
+
+    return theTestHarness.run();
+}
