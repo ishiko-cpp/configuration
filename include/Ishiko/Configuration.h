92fcccb24f4c900005b9e5e6082a666814bae6b5
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-06-21 21:34:48 +0100

    Add files

---
+++ include/Ishiko/Configuration.h
@@ -0,0 +1,12 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/configuration/blob/main/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_CONFIGURATION_H_
+#define _ISHIKO_CONFIGURATION_H_
+
+#include "Process/linkoptions.h"
+
+#endif
