1013856309f3b94b3978befaef78b989847f76f9
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-06-26 15:51:58 +0100

    Implement command line utilities (#1)

---
+++ include/Ishiko/Configuration/CommandLineSpecification.h
@@ -0,0 +1,37 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/configuration/blob/main/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_CONFIGURATION_COMMANDLINESPECIFICATION_H_
+#define _ISHIKO_CONFIGURATION_COMMANDLINESPECIFICATION_H_
+
+#include <map>
+#include <string>
+
+namespace Ishiko
+{
+namespace Configuration
+{
+
+class CommandLineSpecification
+{
+public:
+    struct OptionDetails
+    {
+        std::string defaultValue;
+    };
+
+    void addNamedOption(const std::string& name, const OptionDetails& details);
+
+    bool find(const std::string& name, OptionDetails& details) const;
+
+private:
+    std::map<std::string, OptionDetails> m_options;
+};
+
+}
+}
+
+#endif
---
+++ tests/src/CommandLineParserTests.h
@@ -0,0 +1,21 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/configuration/blob/main/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_CONFIGURATION_TEST_COMMANDLINEPARSERTESTS_H_
+#define _ISHIKO_CONFIGURATION_TEST_COMMANDLINEPARSERTESTS_H_
+
+#include <Ishiko/Tests/Core.h>
+
+class CommandLineParserTests : public Ishiko::Tests::TestSequence
+{
+public:
+    CommandLineParserTests(const Ishiko::Tests::TestNumber& number, const Ishiko::Tests::TestEnvironment& environment);
+
+private:
+    static void ConstructorTest1(Ishiko::Tests::Test& test);
+};
+
+#endif
