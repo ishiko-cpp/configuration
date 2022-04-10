d098340a7bcd25e662dc5ef9caa02c4ef240e940
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-04-10 18:28:58 +0100

    Update code after changes to dependencies (#11)

--- tests/src/CommandLineParserTests.h
+++ tests/src/CommandLineParserTests.h
@@ -7,7 +7,7 @@
 #ifndef _ISHIKO_CPP_CONFIGURATION_TEST_COMMANDLINEPARSERTESTS_H_
 #define _ISHIKO_CPP_CONFIGURATION_TEST_COMMANDLINEPARSERTESTS_H_
 
-#include <Ishiko/Tests/Core.hpp>
+#include <Ishiko/TestFramework/Core.hpp>
 
 class CommandLineParserTests : public Ishiko::TestSequence
 {
