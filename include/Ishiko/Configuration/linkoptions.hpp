1713d09bbda52f7991d8a09fc46b5e40312cbd3a
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-04-10 18:39:36 +0100

    Update build files

--- include/Ishiko/Configuration/linkoptions.hpp
+++ include/Ishiko/Configuration/linkoptions.hpp
@@ -1,16 +1,16 @@
 /*
     Copyright (c) 2022 Xavier Leclercq
     Released under the MIT License
-    See https://github.com/ishiko-cpp/config/blob/main/LICENSE.txt
+    See https://github.com/ishiko-cpp/configuration/blob/main/LICENSE.txt
 */
 
-#ifndef _ISHIKO_CPP_CONFIG_LINKOPTIONS_HPP_
-#define _ISHIKO_CPP_CONFIG_LINKOPTIONS_HPP_
+#ifndef _ISHIKO_CPP_CONFIGURATION_LINKOPTIONS_HPP_
+#define _ISHIKO_CPP_CONFIGURATION_LINKOPTIONS_HPP_
 
 #ifdef _DEBUG
-#pragma comment(lib, "IshikoConfig-d.lib")
+#pragma comment(lib, "IshikoConfiguration-d.lib")
 #else
-#pragma comment(lib, "IshikoConfig.lib")
+#pragma comment(lib, "IshikoConfiguration.lib")
 #endif
 
 #endif
