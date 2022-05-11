c76926d4242224b4d9dc082ad09d47fb9de0d070
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-05-11 19:59:17 +0100

    Updates

---
+++ include/Ishiko/Configuration.hpp
@@ -0,0 +1,15 @@
+/*
+    Copyright (c) 2021-2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/configuration/blob/main/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_CONFIGURATION_HPP_
+#define _ISHIKO_CONFIGURATION_HPP_
+
+#include "Configuration/CommandLineParser.hpp"
+#include "Configuration/CommandLineSpecification.hpp"
+#include "Configuration/Configuration.hpp"
+#include "Configuration/linkoptions.hpp"
+
+#endif
