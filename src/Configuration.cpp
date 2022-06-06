a2e1d9ddee6a5d018e49a48f606cb0909e5a1360
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-06-06 14:57:24 +0100

    Support Configuration items inside another Configuration (#25)

--- src/Configuration.cpp
+++ src/Configuration.cpp
@@ -9,22 +9,27 @@
 using namespace Ishiko;
 
 Configuration::Value::Value(const char* value)
-    : boost::variant<std::string, std::vector<std::string>>(value)
+    : boost::variant<std::string, std::vector<std::string>, boost::recursive_wrapper<Configuration>>(value)
 {
 }
 
 Configuration::Value::Value(const std::string& value)
-    : boost::variant<std::string, std::vector<std::string>>(value)
+    : boost::variant<std::string, std::vector<std::string>, boost::recursive_wrapper<Configuration>>(value)
 {
 }
 
 Configuration::Value::Value(std::string&& value)
-    : boost::variant<std::string, std::vector<std::string>>(value)
+    : boost::variant<std::string, std::vector<std::string>, boost::recursive_wrapper<Configuration>>(value)
 {
 }
 
 Configuration::Value::Value(const std::vector<std::string>& value)
-    : boost::variant<std::string, std::vector<std::string>>(value)
+    : boost::variant<std::string, std::vector<std::string>, boost::recursive_wrapper<Configuration>>(value)
+{
+}
+
+Configuration::Value::Value(const Configuration& value)
+    : boost::variant<std::string, std::vector<std::string>, boost::recursive_wrapper<Configuration>>(value)
 {
 }
 
@@ -43,6 +48,11 @@ const std::vector<std::string>& Configuration::Value::asStringArray() const
     return boost::get<std::vector<std::string>>(*this);
 }
 
+const Configuration& Configuration::Value::asConfiguration() const
+{
+    return boost::get<Configuration>(*this);
+}
+
 size_t Configuration::size() const
 {
     return m_options.size();
@@ -53,13 +63,41 @@ const Configuration::Value& Configuration::value(const std::string& name) const
     return m_options.at(name);
 }
 
-const Configuration::Value& Configuration::valueOrDefault(const std::string& name,
-    const Value& defaultValue) const noexcept
+const std::string& Configuration::valueOrDefault(const std::string& name,
+    const std::string& defaultValue) const noexcept
 {
-    std::map<std::string, Value>::const_iterator  it = m_options.find(name);
+    std::map<std::string, Value>::const_iterator it = m_options.find(name);
     if (it != m_options.end())
     {
-        return it->second;
+        return it->second.asString();
+    }
+    else
+    {
+        return defaultValue;
+    }
+}
+
+const std::vector<std::string>& Configuration::valueOrDefault(const std::string& name,
+    const std::vector<std::string>& defaultValue) const noexcept
+{
+    std::map<std::string, Value>::const_iterator it = m_options.find(name);
+    if (it != m_options.end())
+    {
+        return it->second.asStringArray();
+    }
+    else
+    {
+        return defaultValue;
+    }
+}
+
+const Configuration& Configuration::valueOrDefault(const std::string& name,
+    const Configuration& defaultValue) const noexcept
+{
+    std::map<std::string, Value>::const_iterator it = m_options.find(name);
+    if (it != m_options.end())
+    {
+        return it->second.asConfiguration();
     }
     else
     {
@@ -78,6 +116,21 @@ const Configuration::Value* Configuration::valueOrNull(const std::string& name) 
     {
         return nullptr;
     }
+}
+
+void Configuration::set(const std::string& name, const std::string& value)
+{
+    m_options[name] = Value(value);
+}
+
+void Configuration::set(const std::string& name, const std::vector<std::string>& value)
+{
+    m_options[name] = Value(value);
+}
+
+void Configuration::set(const std::string& name, const Configuration& value)
+{
+    m_options[name] = Value(value);
 }
 
 void Configuration::set(const std::string& name, const Value& value)
