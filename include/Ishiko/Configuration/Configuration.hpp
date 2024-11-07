2977a4053a88a063a665e193e7db964a53d7b643
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2024-11-07 23:07:09 +0000

    Updates

--- include/Ishiko/Configuration/Configuration.hpp
+++ include/Ishiko/Configuration/Configuration.hpp
@@ -1,11 +1,8 @@
-/*
-    Copyright (c) 2022 Xavier Leclercq
-    Released under the MIT License
-    See https://github.com/ishiko-cpp/configuration/blob/main/LICENSE.txt
-*/
+// SPDX-FileCopyrightText: 2000-2024 Xavier Leclercq
+// SPDX-License-Identifier: BSL-1.0
 
-#ifndef _ISHIKO_CONFIGURATION_CONFIGURATION_HPP_
-#define _ISHIKO_CONFIGURATION_CONFIGURATION_HPP_
+#ifndef GUARD_ISHIKO_CPP_CONFIGURATION_CONFIGURATION_HPP
+#define GUARD_ISHIKO_CPP_CONFIGURATION_CONFIGURATION_HPP
 
 #include <boost/variant.hpp>
 #include <map>
@@ -15,51 +12,49 @@
 
 namespace Ishiko
 {
-
-class Configuration
-{
-public:
-    class Value : public boost::variant<std::string, std::vector<std::string>, boost::recursive_wrapper<Configuration>>
+    class Configuration
     {
     public:
-        enum class Type
+        class Value : public boost::variant<std::string, std::vector<std::string>, boost::recursive_wrapper<Configuration>>
         {
-            string = 0,
-            stringArray = 1,
-            configuration = 2,
+        public:
+            enum class Type
+            {
+                string = 0,
+                stringArray = 1,
+                configuration = 2,
+            };
+
+            Value() = default;
+            explicit Value(const char* value);
+            explicit Value(const std::string& value);
+            explicit Value(std::string&& value);
+            explicit Value(const std::vector<std::string>& value);
+            explicit Value(const Configuration& value);
+
+            Type type() const;
+            const std::string& asString() const;
+            const std::vector<std::string>& asStringArray() const;
+            const Configuration& asConfiguration() const;
         };
 
-        Value() = default;
-        explicit Value(const char* value);
-        explicit Value(const std::string& value);
-        explicit Value(std::string&& value);
-        explicit Value(const std::vector<std::string>& value);
-        explicit Value(const Configuration& value);
+        size_t size() const;
 
-        Type type() const;
-        const std::string& asString() const;
-        const std::vector<std::string>& asStringArray() const;
-        const Configuration& asConfiguration() const;
+        const Value& value(const std::string& name) const;
+        const std::string& valueOrDefault(const std::string& name, const std::string& defaultValue) const noexcept;
+        const std::vector<std::string>& valueOrDefault(const std::string& name,
+            const std::vector<std::string>& defaultValue) const noexcept;
+        const Configuration& valueOrDefault(const std::string& name, const Configuration& defaultValue) const noexcept;
+        const Value* valueOrNull(const std::string& name) const noexcept;
+
+        void set(const std::string& name, const std::string& value);
+        void set(const std::string& name, const std::vector<std::string>& value);
+        void set(const std::string& name, const Configuration& value);
+        void set(const std::string& name, const Value& value);
+
+    private:
+        std::map<std::string, Value> m_options;
     };
-
-    size_t size() const;
-
-    const Value& value(const std::string& name) const;
-    const std::string& valueOrDefault(const std::string& name, const std::string& defaultValue) const noexcept;
-    const std::vector<std::string>& valueOrDefault(const std::string& name,
-        const std::vector<std::string>& defaultValue) const noexcept;
-    const Configuration& valueOrDefault(const std::string& name, const Configuration& defaultValue) const noexcept;
-    const Value* valueOrNull(const std::string& name) const noexcept;
-
-    void set(const std::string& name, const std::string& value);
-    void set(const std::string& name, const std::vector<std::string>& value);
-    void set(const std::string& name, const Configuration& value);
-    void set(const std::string& name, const Value& value);
-
-private:
-    std::map<std::string, Value> m_options;
-};
-
 }
 
 #endif
